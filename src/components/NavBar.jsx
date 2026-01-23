8deafa8357d4efa2861d510d653e96a2f541460c
epixjayant <jayantkumara555@gmail.com>
2026-01-23 14:19:47 +0530

    first repo commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <img src='./../assets/image.png' alt='logo image' />
+    </div>
+  );
+};
+
+export default NavBar;
