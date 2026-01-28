79acfab526f0466e58c7d180aa91b8cf28c59433
epixjayant <jayantkumara555@gmail.com>
2026-01-29 03:11:26 +0530

    NavBar created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-
+import "./Navbar.css";
 const NavBar = () => {
   return (
-    <div>
-      <img src='./../assets/image.png' alt='logo image' />
+    <div Class='navbar'>
+      <div className='logo'>
+        <img
+          src='/logo.png'
+          alt='logo image'
+          className='h-16 w-16 bg-amber-50'
+        />
+        <h1>PlantAI</h1>
+      </div>
+      <div class='nav-link'>
+        <ul>Home</ul>
+        <ul>Scan</ul>
+        <ul>Contact</ul>
+        <ul>about</ul>
+      </div>
     </div>
   );
 };
